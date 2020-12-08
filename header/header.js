d03d5f8d29824638e84330c6498c4a3bb9736add
mondobizz <caseyjhatley@yahoo.com>
2020-12-08 15:24:26 -0500

    sets up layout with styles

---
+++ header/header.js
@@ -0,0 +1,15 @@
+export default function Header(props) {
+    return (
+        <header>
+            <h1>My Project</h1>
+            <nav>
+                <ul>
+                    <li>Logout</li>
+                    <li>Dashboard</li>
+                    <li>Signup</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
